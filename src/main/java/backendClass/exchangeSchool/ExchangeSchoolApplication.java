c9c108c9efa02d208c89a051e24974f5a8732603
minjung04 <clover040804@gmail>
2024-04-23 11:41:16 +0900

    프로젝트 업로드 커밋

---
+++ src/main/java/backendClass/exchangeSchool/ExchangeSchoolApplication.java
@@ -0,0 +1,13 @@
+package backendClass.exchangeSchool;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ExchangeSchoolApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ExchangeSchoolApplication.class, args);
+	}
+
+}
