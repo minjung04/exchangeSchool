098181344aebe27303b5dd10e2b0ed5e00fa7432
minjung04 <clover040804@gmail.com>
2024-05-03 00:12:04 +0900

    matching.js 동적구현 완료

--- src/main/resources/static/js/matching.js
+++ src/main/resources/static/js/matching.js
@@ -1,18 +1,18 @@
 
-let cnt = 0;
+let cnt = 1;
 function nextbtn(){
 
     //사용자 정보 변수에 담기
-    if(cnt == 0){
+    if(cnt == 1){
         let user_num = document.getElementsByClassName('user_num')[0].value;
         let user_name = document.getElementsByClassName('user_name')[0].value;
         console.log(user_name, user_num);
 
         // 정보 input 안보이게하고 다음으로
-        let info = document.getElementsByClassName('info-container')[0];
         let q1 = document.getElementById('q1');
-        info.style.display = 'none';
-        q1.style.display = 'block';
+        let q2 = document.getElementById('q2');
+        q1.style.display = 'none';
+        q2.style.display = 'block';
 
         //이전 버튼 생성
         let prebtn = document.getElementsByClassName('prebtn')[0];
@@ -21,27 +21,19 @@ function nextbtn(){
         //cnt증가
         cnt += 1;
         console.log(cnt);
-    }else if(cnt == 1){
-        //체크값 가져옴
-        langCheck();
-
-        let q1 = document.getElementById('q1');
-        let q2 = document.getElementById('q2');
-        q1.style.display = 'none';
-        q2.style.display = 'block';
-        cnt++;
-
     }else if(cnt == 2){
         //체크값 가져옴
-        masterCheck();
+        langCheck();
 
         let q2 = document.getElementById('q2');
         let q3 = document.getElementById('q3');
         q2.style.display = 'none';
         q3.style.display = 'block';
         cnt++;
+
     }else if(cnt == 3){
-        loaCheck();
+        //체크값 가져옴
+        masterCheck();
 
         let q3 = document.getElementById('q3');
         let q4 = document.getElementById('q4');
@@ -49,32 +41,44 @@ function nextbtn(){
         q4.style.display = 'block';
         cnt++;
     }else if(cnt == 4){
-        levelCheck();
+        loaCheck();
 
         let q4 = document.getElementById('q4');
         let q5 = document.getElementById('q5');
         q4.style.display = 'none';
         q5.style.display = 'block';
         cnt++;
-
     }else if(cnt == 5){
-        gradeCheck();
+        levelCheck();
 
         let q5 = document.getElementById('q5');
         let q6 = document.getElementById('q6');
         q5.style.display = 'none';
         q6.style.display = 'block';
         cnt++;
+
     }else if(cnt == 6){
-        fallCheck();
-        //제출 버튼 생성
-        let resultbtn = document.getElementsByClassName('resultbtn')[0];
-        resultbtn.style.display='block';
+        gradeCheck();
 
         let q6 = document.getElementById('q6');
         let q7 = document.getElementById('q7');
         q6.style.display = 'none';
         q7.style.display = 'block';
+        cnt++;
+    }else if(cnt == 7){
+        fallCheck();
+        //제출 버튼 생성
+        let resultbtn = document.getElementsByClassName('resultbtn')[0];
+        resultbtn.style.display='block';
+
+        let q7 = document.getElementById('q7');
+        let q8 = document.getElementById('q8');
+        q7.style.display = 'none';
+        q8.style.display = 'block';
+
+        //다음 버튼 없애기
+        let nextbtn = document.getElementsByClassName('nextbtn')[0];
+        nextbtn.style.display = 'none';
         cnt++;
     }
 
@@ -84,19 +88,33 @@ function prebtn(){
     cnt--;
 
     //이게 전꺼
-    let content = 'q'+String(cnt);
+    let createContent = 'q'+String(cnt);
     //이게 지금꺼
-    let nextContent = 'q'+String(cnt+1);
+    let noneContent = 'q'+String(cnt+1);
     console.log(cnt);
-    console.log('content : ' ,content);
-    console.log('nextcontent : ' ,nextContent);
+    console.log('create : ' ,createContent);
+    console.log('none : ' ,noneContent);
 
-    let none_content = document.getElementsByClassName(eval(""+"content"+""))[0];
+    let none_content = document.getElementById(noneContent);
     console.log("none_content : ",none_content);
-    none_content.style.display = 'block';
+    none_content.style.display = 'none';
 
-    let blockContent = document.getElementsByClassName(eval('nextContent'))[0];
-    blockContent.style.display = 'none';
+    let blockContent = document.getElementById(createContent);
+    blockContent.style.display = 'block';
+    console.log("create_content : ",blockContent);
+
+    //이전 질문이 없을 때
+    if(cnt == 1){
+        let prebtn = document.getElementsByClassName('prebtn')[0];
+        prebtn.style.display = 'none';
+    }else if(cnt == 7){
+        let resultbtn = document.getElementsByClassName('resultbtn')[0];
+        resultbtn.style.display = 'none';
+
+        let nextbtn = document.getElementsByClassName('nextbtn')[0];
+        nextbtn.style.display = 'block';
+    }
+
 }
 
 function langCheck(){
